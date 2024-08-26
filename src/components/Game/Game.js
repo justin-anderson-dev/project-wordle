dcf0be23bef28439adc10bb09cf88d3f7418c01b
JUSTIN ANDERSON <justin.anderson@slalom.com>
2024-08-26 14:55:08 -0700

    Exercise 5 and cleanup

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput';
 import GuessResults from '../GuessResults/GuessResults';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,11 +13,47 @@ console.info({ answer });
 
 function Game() {
 	const [guesses, setGuesses] = React.useState([]);
+	const [gameStatus, setGameStatus] = React.useState('running');
+
+	function handleAddGuess(newGuess) {
+		const nextGuesses = [...guesses, newGuess];
+		setGuesses(nextGuesses);
+		console.log('guess: ', newGuess);
+
+		// note: guesses array at this point is stale.
+		if (newGuess === answer) {
+			setGameStatus('won');
+		} else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+			setGameStatus('lost');
+		}
+	}
 
 	return (
 		<>
-			<GuessResults guesses={guesses} answer={answer} />
-			<GuessInput guesses={guesses} setGuesses={setGuesses} />
+			<GuessResults
+				guesses={guesses}
+				answer={answer}
+				setGameStatus={setGameStatus}
+			/>
+			<GuessInput
+				handleAddGuess={handleAddGuess}
+				gameStatus={gameStatus}
+			/>
+			{gameStatus === 'won' && (
+				<div className='happy banner'>
+					<p>
+						<strong>Congratulations!</strong> Got it in
+						<strong> {guesses.length} guesses</strong>.
+					</p>
+				</div>
+			)}
+			{gameStatus === 'lost' && (
+				<div className='sad banner'>
+					<p>
+						Sorry, the correct answer is <strong>{answer}</strong>.
+					</p>
+				</div>
+			)}
 		</>
 	);
 }
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,7 +3,7 @@ import Guess from '../Guess/Guess';
 import { range } from '../../utils';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
-function GuessResults({ guesses, answer }) {
+function GuessResults({ guesses, answer, setGameStatus }) {
 	console.log('guesses: ', guesses);
 	const indexes = range(0, NUM_OF_GUESSES_ALLOWED);
 	console.log('indexes: ', indexes);
